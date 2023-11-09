96563105bf7aaebf7287497849dd473ce7a8cf8f
Aye Zin Zin Aung <ayezin2aung101120@gmail.com>
2023-11-09 16:42:47 +0630

    ui change login logout

---
+++ resources/views/frontend/home.blade.php
@@ -0,0 +1,11 @@
+@extends('frontend.layout.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-3">
+            <h1>Home Page</h1> 
+        </div>
+    </div>
+</div>
+@endsection
